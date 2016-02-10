ccd1d8c7d16bbd50364e4ddc1516c0e1e6da059a
Brett Steckman <eveningside@gmail.com>
2016-02-10 13:49:19 -0700

    properties are going to member item but not firebase yet

--- app/components/dashboard/dashboard-component.js
+++ app/components/dashboard/dashboard-component.js
@@ -1,6 +1,8 @@
 app.controller('DashboardController', function($rootScope, $scope){
+    $rootScope.members = [];
+    
     $rootScope.member= {
-        username: '',
+        userName: '',
 		firstName: '',
 		lastName: '',
 		imgUrl:  '',
@@ -9,25 +11,32 @@ app.controller('DashboardController', function($rootScope, $scope){
 		websiteUrl: '',
 		github: '',
 		twitterHandle: '',
-		favoriteTags: [tag],
+		favoriteTags: [],
 		
         
-        accountCreated: date,
-		upVotes: number,
-		downVotes: number,
-		reputation: number,
-		questions: [questionId],
-		answers: [answerId],
-		comments: [commentId]
+        // accountCreated: date,
+		// upVotes: number,
+		// downVotes: number,
+		// reputation: number,
+		// questions: [questionId],
+		// answers: [answerId],
+		// comments: [commentId]
     }
-	/**
-	 * To edit the member object you must go through $rootScope.member
-	 * $rootScope.member is a $firebaseObject from AngularFire 
-	 * To see the methods at your disposal look here
-	 * https://www.firebase.com/docs/web/libraries/angular/api.html#angularfire-firebaseobject
-	 * 
-	 * Don't forget to call $rootScope.member.$save() after making changes to the $rootScope.member object
-	 * 
-	 * */
+    
+    $rootScope.submitForm = function(){
+        if($scope.member){
+            $rootScope.members.push($rootScope.member);
+            
+        }
+    }
+
+	//  * To edit the member object you must go through $rootScope.member
+	//  * $rootScope.member is a $firebaseObject from AngularFire 
+	//  * To see the methods at your disposal look here
+	//  * https://www.firebase.com/docs/web/libraries/angular/api.html#angularfire-firebaseobject
+	//  * 
+	//  * Don't forget to call $rootScope.member.$save() after making changes to the $rootScope.member object
+	//  * 
+	//  * */
 	
 });
